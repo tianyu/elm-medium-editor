a0f401c6a8853bff846ce6003bce6daa5a95ff75
Tianyu Zhu <tian.tian098@gmail.com>
2016-10-11 07:44:50 -0400

    Add documentation in preparation for release

--- src/MediumEditor.elm
+++ src/MediumEditor.elm
@@ -11,6 +11,27 @@ module MediumEditor
         , onBlur
         )
 
+{-| Rich text editing using the contenteditable API.
+
+This is an Elm wrapper around
+[a Javascript clone of Medium.com's editor](github.com/yabwe/medium-editor).
+It allows for rich-text editing in a web browser environment using the
+contenteditable API.
+
+# Widgets
+@docs editor, editorWithOptions
+
+# Attributes
+@docs placeholder
+
+# Events
+@docs onInput, onBlur
+
+# Options
+@docs Options, defaultOptions
+@docs ToolbarOptions, defaultToolbarOptions
+-}
+
 import Html exposing (Html, Attribute)
 import Html.Attributes exposing (attribute)
 import Html.Events exposing (on)
@@ -18,44 +39,68 @@ import Json.Decode as Decode exposing (Decoder)
 import Native.MediumEditor
 
 
+{-| Editor options
+
+- toolbar: Customizes the editor's toolbar. Set this to `Nothing` to disable the
+  toolbar altogether. Disabling the toolbar will also disable anchor previews.
+-}
 type alias Options =
     { toolbar : Maybe ToolbarOptions }
 
 
+{-| Editor toolbar options
+-}
 type alias ToolbarOptions =
     {}
 
 
+{-| Default editor options
+
+- toolbar: The toolbar is enabled and set to
+[the default options](#defaultToolbarOptions).
+-}
 defaultOptions : Options
 defaultOptions =
     { toolbar = Just defaultToolbarOptions }
 
 
+{-| Default toolbar options
+-}
 defaultToolbarOptions : ToolbarOptions
 defaultToolbarOptions =
     {}
 
 
+{-| Create an editor with [default options](#defaultOptions)
+-}
 editor : List (Attribute msg) -> String -> Html msg
 editor =
     editorWithOptions defaultOptions
 
 
+{-| Create an editor with custom options
+-}
 editorWithOptions : Options -> List (Attribute msg) -> String -> Html msg
 editorWithOptions =
     Native.MediumEditor.editor
 
 
+{-| Customize the editor's placeholder. The default is `Type your text`.
+-}
 placeholder : String -> Attribute msg
 placeholder =
     attribute "data-placeholder"
 
 
+{-| Sends an event with the editor's innerHTML on every input event.
+-}
 onInput : (String -> msg) -> Attribute msg
 onInput =
     on "input" << sendInnerHTML
 
 
+{-| Sends an event with the editor's innerHTML when the editor loses focus.
+-}
 onBlur : (String -> msg) -> Attribute msg
 onBlur =
     on "blur" << sendInnerHTML
